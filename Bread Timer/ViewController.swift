b9e949c74a6617a12ab0f1bf823fbda9438f4cf8
Tim Darragh <tdarragh@gmail.com>
2020-05-24 13:07:14 -0400

    Initial Commit

---
+++ Bread Timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bread Timer
+//
+//  Created by Tim Darragh on 5/24/20.
+//  Copyright © 2020 Tim Darragh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
